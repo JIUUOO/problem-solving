b786a4fbaa7ff69191050249e5451a3d4124acbe
JIUUOO <nninjiuuoo@gmail.com>
2025-03-03 01:39:19 +0900

    [Baekjoon/Bronze_5] ✅ Solve 14681

---
+++ Baekjoon/Bronze_5/14681.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int x, y;
+  int quadrant;
+
+  cin >> x >> y;
+
+  if (x > 0 && y > 0) {
+    quadrant = 1;
+  } else if (x < 0 && y > 0) {
+    quadrant = 2;
+  } else if (x < 0 && y < 0) {
+    quadrant = 3;
+  } else if (x > 0 && y < 0) {
+    quadrant = 4;
+  } else {
+    return -1;
+  }
+
+  cout << quadrant << endl;
+}
