b05f86bd973605fbbdfa2c3d0277a7ec291ddfd1
JIUUOO <nninjiuuoo@gmail.com>
2025-03-02 13:21:45 +0900

    [Baekjoon/Bronze_5] ✅ Solve 10998

---
+++ Baekjoon/Bronze_5/10998.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a, b;
+
+  cin >> a >> b;
+
+  if (!(a > 0 && a < 10) || !(b > 0 && b < 10))
+    cout << "입력값이 조건에 맞지 않아요!" << endl;
+  else
+    cout << a * b << endl;
+}
